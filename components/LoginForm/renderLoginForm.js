95584d5b07b7c38e74d93f2d83e0530ce2fd078c
michalinaprzybyl <michalinaprzybyl@interia.pl>
2022-11-26 17:56:58 +0100

    add RegisterForm and LoginForm

---
+++ components/LoginForm/renderLoginForm.js
@@ -0,0 +1,20 @@
+// W funkcji:
+// 1. Stwórz element <form> i nadaj mu id 'login-form'
+// 2. Stwórz element <input>, nadaj mu type 'email', id 'input-email', placeholder 'email'
+// 3. Stwórz element <input>, nadaj mu type 'password', id 'input-password', placeholder 'password'
+// 4. Stwórz element <button>, nadaj mu type 'submit', textContent 'Sign in'
+// 5. Podepnij oba inputy i button do form.
+// 6. Na form nakładacie event listener (na submit).
+// W środku event listenera:
+// 7. Ściągnij wartości inputów email i password, zapisz do zmiennych (.value)
+// 8. Wywołanie funkcji signInWithEmailAndPassword, funkcja przyjmuje 3 argumenty:
+// - obiekt auth (zaimportuj z firebaseConfig.js)
+// - email (który przed chwilą ściągnąłeś z inputa)
+// - password (też z inputa)
+// 9. Do wywołania poprzedniej funkcji dopisz thena, będzie on przyjmował parametr creds, w środku thena console.log(creds), console.log('Zalogowano') i wywołanie renderHomePage() (wcześniej trzeba zaimportować), dodaj metode catch i console.log('Error')
+// KONIEC EL
+// 10. Zwróć element form z głównej funkcji przy pomocy return
+
+export default function () {
+
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+import renderHomePage from "./components/HomePage/renderHomePage.js";   // to jest export default 
+import renderRegisterForm from "./components/RegisterForm/renderRegisterForm.js";
+
+// Selecting navbar anchors
+const homeButton = document.getElementById("home-anchor");
+const todosButton = document.getElementById("todos-anchor");
+const aboutButton = document.getElementById("about-anchor");
+const publicButton = document.getElementById("public-anchor");
+const loginButton = document.getElementById("login-anchor");
+
+// Rendering the home page on initial page load
+renderHomePage();
+renderRegisterForm();
+
+// Navbar buttons listeners
+// Home button
+homeButton.addEventListener("click", () => {
+    renderHomePage();
+});
+// można też zapisaćw ten sposób:
+// homeButton.addEventListener("click", renderHomePage);
